5a50a710231d957839197c407353c3d2928ceb56
Jason Pham <pploc21@clc.fitus.edu.vn>
2026-08-09 12:26:55 +0700

    refactor: remove unused ComponentScan and use package-private main method

--- src/main/java/com/gym/plans/MsGymPlansApplication.java
+++ src/main/java/com/gym/plans/MsGymPlansApplication.java
@@ -2,13 +2,11 @@ package com.gym.plans;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.gym.plans", "com.gym.common"})
 public class MsGymPlansApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(MsGymPlansApplication.class, args);
     }
 }
